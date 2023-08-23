61afa79b025e762f45be345dffe9944071c36d99
Kakackle <markowskimaciej98@gmail.com>
2023-08-23 20:32:45 +0200

    nav and styles

--- insta/urls.py
+++ insta/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("instaapp.urls", namespace="instaapp")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
