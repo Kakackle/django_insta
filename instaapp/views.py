3e8ac520d4d5c11db197cdacfeb27de6239c5f5f
Kakackle <markowskimaciej98@gmail.com>
2023-08-26 12:00:23 +0200

    added scripts to static and infinite scroll to home view

--- instaapp/views.py
+++ instaapp/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from instaapp.models import Post, Comment, Tag
 from django.contrib.auth.models import User
 from users.models import UserProfile, Following
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def home_view(request):
     # posty z wykluczeniem postow wlasnych autora
-    posts = Post.objects.exclude(author=request.user.id)
+    posts_all = Post.objects.exclude(author=request.user.id)
     if (request.user.is_authenticated):
         followed_users_pks = list(request.user.followed.all()\
                                 .values_list('followed_user', flat=True))
@@ -14,6 +15,17 @@ def home_view(request):
     else:
         followed_users = []
     # print('followed: ', followed_users)
+    
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts_all, 4)
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    
     context = {
         "posts": posts,
         "user": request.user,
